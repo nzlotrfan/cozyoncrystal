afea99e09543c2668190fa3a7a0d977828315e27
Reuben Simpson <useyourtimewell@gmail.com>
2025-08-19 08:40:53 +1200

    Updated review count and also packages

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
             </div>
             <div className={styles.cardRight}>
               <div className={styles.cardReviews}>
-                <p>8</p>
+                <p>9</p>
                 <p>Reviews</p>
               </div>
               <div className={styles.cardRating}>
